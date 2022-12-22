efae761d7acb98646ea9920c5b4f0d537d165a22
Soumyodip Sutradhar <74486329+Samsand-5@users.noreply.github.com>
2022-12-22 13:53:23 +0530

    Create Code snippet in WeatherActivity.java

---
+++ Code snippet in WeatherActivity.java
@@ -0,0 +1,7 @@
+boolean isRaining = true;
+if (isRaining) {
+     Log.v("WeatherActivity", "It's raining, better bring an umbrella.");
+} else {
+     Log.v("WeatherActivity", "It's unlikely to rain.");
+}
+Log.v("WeatherActivity", "Thank you for using the WhetherWeather App.");
